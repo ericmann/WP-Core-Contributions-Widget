c6e3f61eda9891381d9e2b9577189b74751b8921
Eric Mann <eric@eamann.com>
2011-12-07 18:53:17 -0800

    Actually do the work to grab data from Trac and cache it in a transient.

--- lib/class.wp-core-contributions-widget.php
+++ lib/class.wp-core-contributions-widget.php
@@ -11,11 +11,61 @@ class WP_Core_Contributions_Widget extends WP_Widget {
 		$this->WP_Widget( false, 'WP Core Contributions', $widget_ops );
 	}
 
-	function widget( $args, $instance ){
+	function form( $instance ) {
+		if ( $instance ) {
+			$title = esc_attr( $instance[ 'title' ] );
+			$tracUser = esc_attr( $instance[ 'trac-user' ] );
+		} else {
+			$title = __( 'New title', 'wp-core-contributions-widget' );
+			$tracUser = __( 'Trac Username', 'wp-core-contributions-widget' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 
+		<label for="<?php echo $this->get_field_id('trac-user'); ?>"><?php _e('Trac Username:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('trac-user'); ?>" name="<?php echo $this->get_field_name('trac-user'); ?>" type="text" value="<?php echo $tracUser; ?>" />
+		</p>
+		<?php
 	}
 
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['trac-user'] = strip_tags($new_instance['trac-user']);
+		return $instance;
+	}
 
+	function widget( $args, $instance ){
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$user = $instance['trac-user'];
+
+		echo $before_widget;
+
+		if ( $title ) {
+			echo $before_title . $title . $after_title;
+		}
+
+		// Widget content
+		$items = WP_Core_Contributions::get_items($user);
+
+		echo '<ul>';
+
+		foreach ($items as $item) {
+			echo '<li>';
+
+			echo '<a href="' . $item['link'] . '">[' . $item['changeset'] . ']</a> ';
+			echo 'for <a href="http://core.trac.wordpress.org/ticket/' . $item['ticket'] . '">#' . $item['ticket'] . '</a>';
+
+			echo '</li>';
+		}
+
+		echo '</ul>';
+
+		echo $after_widget;
+	}
 }
 
 endif;
--- lib/class.wp-core-contributions.php
+++ lib/class.wp-core-contributions.php
@@ -10,6 +10,36 @@ class WP_Core_Contributions{
 	public static function register_widget() {
 		register_widget('WP_Core_Contributions_Widget');
 	}
+
+	public static function get_items( $username ) {
+		if($username == null) return array();
+
+		if( false === ( $formatted = get_transient( 'wp-core-contributions' ) ) ) {
+
+			$results = wp_remote_retrieve_body(wp_remote_get('https://core.trac.wordpress.org/search?q=props+' . $username . '&noquickjump=1&changeset=on', array('sslverify'=>false)));
+
+			$pattern = '/<dt><a href="(.*?)" class="searchable">\[(.*?)\]: (.*?)<\/a><\/dt>\n\s*<dd class="searchable">.*ixes #(.*?)\n?<\/dd>/';
+
+			preg_match_all($pattern, $results, $matches, PREG_SET_ORDER);
+
+			$formatted = array();
+
+			foreach($matches as $match) {
+				array_shift($match);
+				$newMatch = array(
+					'link' => 'https://core.trac.wordpress.org' . $match[0],
+					'changeset' => intval($match[1]),
+					'description' => $match[2],
+					'ticket' => intval($match[3])
+				);
+				array_push($formatted, $newMatch);
+			}
+
+			set_transient( 'wp-core-contributions', $formatted, 60 * 60 * 12 );
+		}
+
+		return $formatted;
+	}
 }
 
 endif;
