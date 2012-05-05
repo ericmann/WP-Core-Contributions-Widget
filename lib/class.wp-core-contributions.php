08cdf89fdb934c88f9704e07f11368d5df901cc3
Konstantin Obenland <obenland@gmx.de>
2012-05-05 12:54:43 +0300

    Manage query strings with add_query_arg(), swap values when checking for a user name, general WordPress Coding Guidelines brushup

--- lib/class.wp-core-contributions.php
+++ lib/class.wp-core-contributions.php
@@ -1,7 +1,7 @@
 <?php
 if ( ! class_exists('WP_Core_Contributions') ) :
 
-class WP_Core_Contributions{
+class WP_Core_Contributions {
 	public static function init() {
 		add_action( 'init',         array( 'WP_Core_Contributions', 'wp_init' ) );
 		add_action( 'widgets_init', array( 'WP_Core_Contributions', 'register_widget' ) );
@@ -12,32 +12,36 @@ class WP_Core_Contributions{
 	}
 
 	public static function register_widget() {
-		register_widget('WP_Core_Contributions_Widget');
-		register_widget('WP_Codex_Contributions_Widget');
+		register_widget( 'WP_Core_Contributions_Widget' );
+		register_widget( 'WP_Codex_Contributions_Widget' );
 	}
 
 	public static function get_items( $username ) {
-		if($username == null) return array();
+		if ( null == $username ) return array();
 
-		if( false === ( $formatted = get_transient( 'wp-core-contributions-' . $username ) ) ) {
-
-			$results = wp_remote_retrieve_body(wp_remote_get('https://core.trac.wordpress.org/search?q=props+' . $username . '&noquickjump=1&changeset=on', array('sslverify'=>false)));
+		if ( false === ( $formatted = get_transient( 'wp-core-contributions-' . $username ) ) ) {
+			$results_url = add_query_arg( array(
+					'q'				=>	'props+' . $username,
+					'noquickjump'	=>	'1',
+					'changeset'		=>	'on'
+			), 'https://core.trac.wordpress.org/search' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
 
 			$pattern = '/<dt><a href="(.*?)" class="searchable">\[(.*?)\]: ((?s).*?)<\/a><\/dt>\n\s*(<dd class="searchable">.*\n?.*(?:ixes|ee) #(.*?)\n?<\/dd>)?/';
 
-			preg_match_all($pattern, $results, $matches, PREG_SET_ORDER);
+			preg_match_all( $pattern, $results, $matches, PREG_SET_ORDER );
 
 			$formatted = array();
 
-			foreach($matches as $match) {
-				array_shift($match);
-				$newMatch = array(
-					'link' => 'https://core.trac.wordpress.org' . $match[0],
-					'changeset' => intval($match[1]),
-					'description' => $match[2],
-					'ticket' => isset( $match[3] ) ? intval($match[4]) : '',
+			foreach ( $matches as $match ) {
+				array_shift( $match );
+				$new_match = array(
+					'link'			=> 'https://core.trac.wordpress.org' . $match[0],
+					'changeset'		=> intval($match[1]),
+					'description'	=> $match[2],
+					'ticket'		=> isset( $match[3] ) ? intval($match[4]) : '',
 				);
-				array_push($formatted, $newMatch);
+				array_push( $formatted, $new_match );
 			}
 
 			set_transient( 'wp-core-contributions-' . $username, $formatted, 60 * 60 * 12 );
@@ -47,16 +51,21 @@ class WP_Core_Contributions{
 	}
 
 	public static function get_changeset_count( $username ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 
 		if ( false == ( $count = get_transient( 'wp-core-contributions-count-' . $username ) ) ) {
-			$results = wp_remote_retrieve_body(wp_remote_get('https://core.trac.wordpress.org/search?q=props+' . $username . '&noquickjump=1&changeset=on', array('sslverify'=>false)));
+			$results_url = add_query_arg( array(
+				'q'				=>	'props+' . $username,
+				'noquickjump'	=>	'1',
+				'changeset'		=>	'on'
+			), 'https://core.trac.wordpress.org/search' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
 
 			$pattern = '/<meta name="totalResults" content="(\d*)" \/>/';
 
-			preg_match($pattern, $results, $matches);
+			preg_match( $pattern, $results, $matches );
 
-			$count = intval($matches[1]);
+			$count = intval( $matches[1] );
 
 			set_transient( 'wp-core-contributions-count-' . $username, $count, 60 * 60 * 12 );
 		}
@@ -65,14 +74,21 @@ class WP_Core_Contributions{
 	}
 	
 	public static function get_codex_items( $username, $limit = 10 ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 		
 		if ( true || false == ( $formatted = get_transient( 'wp-codex-contributions-' . $username ) ) ) {
 			
-			$results_url = 'http://codex.wordpress.org/api.php?action=query&list=usercontribs&ucuser=' . $username . '&uclimit=' . $limit . '&ucdir=older&format=php';
-			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array('sslverify'=>false) ) );
+			$results_url = add_query_arg( array(
+				'action'	=>	'query',
+				'list'		=>	'usercontribs',
+				'ucuser'	=>	$username,
+				'uclimit'	=>	$limit,
+				'ucdir'		=>	'older',
+				'format'	=>	'php'
+			), 'http://codex.wordpress.org/api.php' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
 
-			$raw = unserialize( $results );
+			$raw = maybe_unserialize( $results );
 			
 			/* Expected array format is as follows:
 			 * Array
@@ -98,15 +114,15 @@ class WP_Core_Contributions{
 			
 			foreach( $raw['query']['usercontribs'] as $item ) {
 				$count = 0;
-				$clean_title = preg_replace( '/^Function Reference\//', '', (string)$item['title'], 1, $count );
+				$clean_title = preg_replace( '/^Function Reference\//', '', (string) $item['title'], 1, $count );
 
-				$newItem = array(
-					'title' => $clean_title,
-					'description' => (string)$item['comment'],
-					'revision' => (int)$item['revid'],
-					'function_ref' => $count == 0 ? false : true
+				$new_item = array(
+					'title'			=> $clean_title,
+					'description'	=> (string) $item['comment'],
+					'revision'		=> (int) $item['revid'],
+					'function_ref'	=> (bool) $count
 				);
-				array_push( $formatted, $newItem );
+				array_push( $formatted, $new_item );
 			}
 			
 			set_transient( 'wp-codex-contributions-' . $username, $formatted, 60 * 60 * 12 );
@@ -116,11 +132,17 @@ class WP_Core_Contributions{
 	}
 	
 	public static function get_codex_count( $username ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 
 		if ( false == ( $count = get_transient( 'wp-codex-contributions-count-' . $username ) ) ) {
 			
-			$results_url = 'http://codex.wordpress.org/api.php?action=query&list=users&ususers=' . $username . '&usprop=editcount&format=xml';
+			$results_url = add_query_arg( array(
+				'action'	=>	'query',
+				'list'		=>	'users',
+				'ususers'	=>	$username,
+				'usprop'	=>	'editcount',
+				'format'	=>	'xml'
+			), 'http://codex.wordpress.org/api.php' );
 			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array('sslverify'=>false) ) );
 			
 			/* Expected XML format is as follows:
@@ -135,7 +157,7 @@ class WP_Core_Contributions{
 			 **/
 			
 			$raw = new SimpleXMLElement( $results );
-			$count = (int)$raw->query->users->user["editcount"];
+			$count = (int) $raw->query->users->user["editcount"];
 			
 			set_transient( 'wp-codex-contributions-count-' . $username, $count, 60 * 60 * 12 );
 		}
@@ -145,4 +167,3 @@ class WP_Core_Contributions{
 }
 
 endif;
-?>
