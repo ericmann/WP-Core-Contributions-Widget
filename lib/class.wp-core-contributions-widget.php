64ed79258e84cafad4967e2ee23da4134ca4909c
Konstantin Obenland <obenland@gmx.de>
2012-05-05 12:58:10 +0300

    Adjust default value for display-count in widget() method, Better sanitation for display-count and translated strings, add textdomains to form labels, WordPress Coding Standard brushup

--- lib/class.wp-core-contributions-widget.php
+++ lib/class.wp-core-contributions-widget.php
@@ -18,39 +18,39 @@ class WP_Core_Contributions_Widget extends WP_Widget {
 		if ( $instance && isset( $instance[ 'title' ] ) ) {
 			$title = esc_attr( $instance[ 'title' ] );
 		} else {
-			$title = __( 'WP Core Contributions', 'wp-core-contributions-widget' );
+			$title = esc_attr__( 'WP Core Contributions', 'wp-core-contributions-widget' );
 		}
 		
 		if ( $instance && isset( $instance[ 'trac-user' ] ) ) {
-			$tracUser = esc_attr( $instance[ 'trac-user' ] );
+			$trac_user = esc_attr( $instance[ 'trac-user' ] );
 		} else {
-			$tracUser = __( 'Trac Username', 'wp-core-contributions-widget' );
+			$trac_user = esc_attr__( 'Trac Username', 'wp-core-contributions-widget' );
 		}
 		
 		if ( $instance && isset( $instance[ 'display-count' ] ) ) {
-			$tracCount = esc_attr( $instance[ 'display-count' ] );
+			$trac_count = absint( $instance[ 'display-count' ] );
 		} else {
-			$tracCount = 5;
+			$trac_count = 5;
 		}
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'wp-core-contributions-widget' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
 
-			<label for="<?php echo $this->get_field_id('trac-user'); ?>"><?php _e('Trac Username:'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('trac-user'); ?>" name="<?php echo $this->get_field_name('trac-user'); ?>" type="text" value="<?php echo $tracUser; ?>" />
+			<label for="<?php echo $this->get_field_id( 'trac-user' ); ?>"><?php _e( 'Trac Username:', 'wp-core-contributions-widget' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('trac-user'); ?>" name="<?php echo $this->get_field_name( 'trac-user' ); ?>" type="text" value="<?php echo $trac_user; ?>" />
 
-			<label for="<?php echo $this->get_field_id('display-count'); ?>"><?php _e('Display How Many Tickets?'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('display-count'); ?>" name="<?php echo $this->get_field_name('display-count'); ?>" type="text" value="<?php echo $tracCount; ?>" />
+			<label for="<?php echo $this->get_field_id( 'display-count' ); ?>"><?php _e( 'Display How Many Tickets?', 'wp-core-contributions-widget' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'display-count' ); ?>" name="<?php echo $this->get_field_name( 'display-count' ); ?>" type="text" value="<?php echo $trac_count; ?>" />
 		</p>
 		<?php
 	}
 
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['trac-user'] = strip_tags($new_instance['trac-user']);
-		$instance['display-count'] = strip_tags($new_instance['display-count']);
+		$instance['title']			= strip_tags( $new_instance['title'] );
+		$instance['trac-user']		= strip_tags( $new_instance['trac-user'] );
+		$instance['display-count']	= absint( $new_instance['display-count'] );
 		return $instance;
 	}
 
@@ -58,7 +58,7 @@ class WP_Core_Contributions_Widget extends WP_Widget {
 		extract( $args );
 		$title = apply_filters( 'widget_title', $instance['title'] );
 		$user = $instance['trac-user'];
-		$count = isset($instance['display-count']) ? $instance['display-count'] : 10;
+		$count = isset( $instance['display-count'] ) ? $instance['display-count'] : 5;
 
 		echo $before_widget;
 
@@ -67,8 +67,8 @@ class WP_Core_Contributions_Widget extends WP_Widget {
 		}
 
 		// Widget content
-		$items = array_slice( WP_Core_Contributions::get_items($user), 0, $count );
-		$total = WP_Core_Contributions::get_changeset_count($user);
+		$items = array_slice( WP_Core_Contributions::get_items( $user ), 0, $count );
+		$total = WP_Core_Contributions::get_changeset_count( $user );
 
 		// Include template - can be overriden by a theme!
 		$template_name = 'wp-core-contributions-widget-template.php';
