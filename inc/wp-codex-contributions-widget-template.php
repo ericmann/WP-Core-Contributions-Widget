08165faeef74e3454fea7b84a613bd9fea5bef73
Mike Bijon <mike@etchsoftware.com>
2012-05-19 01:15:42 -0700

    Pull & squash from Eric's origin/master branch. Now v1.2.2 locally

--- inc/wp-codex-contributions-widget-template.php
+++ inc/wp-codex-contributions-widget-template.php
@@ -10,19 +10,26 @@
 	<ul>
 	<?php foreach ( (array) $items as $item ) :
 		$link = 'http://codex.wordpress.org/index.php?title=' . $item['title'] . '&oldid=' . $item['revision'];
+
+		if ( (bool)$item['function_ref'] ) {
 	?>
-		<li><?php printf( __( 'For %1$s' ),
-			'<a href="' . esc_url( $link ) . '">' . esc_html( $item['title'] ) . '</a>'
+		<li><?php printf( __( 'Function: %1$s', 'wp-core-contributions-widget' ),
+			'<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>'
 		); ?></li>
+	<?php } else { ?>
+		<li><?php printf( __( 'For %1$s', 'wp-core-contributions-widget' ),
+			'<a href="' . esc_url( $link ) . '" title="' . esc_html( $item['description'] ) . '">' . esc_html( $item['title'] ) . '</a>'
+		); ?></li>
+	<?php } ?>
 	<?php endforeach; ?>
 	</ul>
 	<p>
 		<a href="<?php echo esc_url( 'http://codex.wordpress.org/Special:Contributions/' . ucfirst( $user ) ); ?>">
 			<?php
 				if ( $total == 2 ) {
-					print( "View both changes in the Codex." );
+					_e( "View both changes in the Codex.", 'wp-core-contributions-widget' );
 				} else {
-					printf( _n( "View the change in the Codex.", "View all %d changes in the Codex.", $total ), $total );
+					printf( _n( "View the change in the Codex.", "View all %d changes in the Codex.", $total, 'wp-core-contributions-widget' ), $total );
 				}
 			?>
 		</a>
--- lib/class.wp-core-contributions.php
+++ lib/class.wp-core-contributions.php
@@ -1,43 +1,47 @@
 <?php
 if ( ! class_exists('WP_Core_Contributions') ) :
 
-class WP_Core_Contributions{
+class WP_Core_Contributions {
 	public static function init() {
 		add_action( 'init',         array( 'WP_Core_Contributions', 'wp_init' ) );
 		add_action( 'widgets_init', array( 'WP_Core_Contributions', 'register_widget' ) );
 	}
 
 	public static function wp_init() {
-		load_plugin_textdomain( 'wp-core-contributions-widget', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+		load_plugin_textdomain( 'wp-core-contributions-widget', false, dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/lang/' );
 	}
 
 	public static function register_widget() {
-		register_widget('WP_Core_Contributions_Widget');
-		register_widget('WP_Codex_Contributions_Widget');
+		register_widget( 'WP_Core_Contributions_Widget' );
+		register_widget( 'WP_Codex_Contributions_Widget' );
 	}
 
 	public static function get_items( $username ) {
-		if($username == null) return array();
+		if ( null == $username ) return array();
 
-		if( false === ( $formatted = get_transient( 'wp-core-contributions-' . $username ) ) ) {
-
-			$results = wp_remote_retrieve_body(wp_remote_get('https://core.trac.wordpress.org/search?q=props+' . $username . '&noquickjump=1&changeset=on', array('sslverify'=>false)));
+		if ( false === ( $formatted = get_transient( 'wp-core-contributions-' . $username ) ) ) {
+			$results_url = add_query_arg( array(
+					'q'				=>	'props+' . $username,
+					'noquickjump'	=>	'1',
+					'changeset'		=>	'on'
+			), 'https://core.trac.wordpress.org/search' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
 
 			$pattern = '/<dt><a href="(.*?)" class="searchable">\[(.*?)\]: ((?s).*?)<\/a><\/dt>\n\s*(<dd class="searchable">.*\n?.*(?:ixes|ee) #(.*?)\n?<\/dd>)?/';
 
-			preg_match_all($pattern, $results, $matches, PREG_SET_ORDER);
+			preg_match_all( $pattern, $results, $matches, PREG_SET_ORDER );
 
 			$formatted = array();
 
-			foreach($matches as $match) {
-				array_shift($match);
-				$newMatch = array(
-					'link' => 'https://core.trac.wordpress.org' . $match[0],
-					'changeset' => intval($match[1]),
-					'description' => $match[2],
-					'ticket' => isset( $match[3] ) ? intval($match[4]) : '',
+			foreach ( $matches as $match ) {
+				array_shift( $match );
+				$new_match = array(
+					'link'			=> 'https://core.trac.wordpress.org' . $match[0],
+					'changeset'		=> intval($match[1]),
+					'description'	=> $match[2],
+					'ticket'		=> isset( $match[3] ) ? intval($match[4]) : '',
 				);
-				array_push($formatted, $newMatch);
+				array_push( $formatted, $new_match );
 			}
 
 			set_transient( 'wp-core-contributions-' . $username, $formatted, 60 * 60 * 12 );
@@ -47,16 +51,21 @@ class WP_Core_Contributions{
 	}
 
 	public static function get_changeset_count( $username ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 
 		if ( false == ( $count = get_transient( 'wp-core-contributions-count-' . $username ) ) ) {
-			$results = wp_remote_retrieve_body(wp_remote_get('https://core.trac.wordpress.org/search?q=props+' . $username . '&noquickjump=1&changeset=on', array('sslverify'=>false)));
+			$results_url = add_query_arg( array(
+				'q'				=>	'props+' . $username,
+				'noquickjump'	=>	'1',
+				'changeset'		=>	'on'
+			), 'https://core.trac.wordpress.org/search' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
 
 			$pattern = '/<meta name="totalResults" content="(\d*)" \/>/';
 
-			preg_match($pattern, $results, $matches);
+			preg_match( $pattern, $results, $matches );
 
-			$count = intval($matches[1]);
+			$count = intval( $matches[1] );
 
 			set_transient( 'wp-core-contributions-count-' . $username, $count, 60 * 60 * 12 );
 		}
@@ -65,44 +74,55 @@ class WP_Core_Contributions{
 	}
 	
 	public static function get_codex_items( $username, $limit = 10 ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 		
-		if ( false == ( $formatted = get_transient( 'wp-codex-contributions-' . $username ) ) ) {
+		if ( true || false == ( $formatted = get_transient( 'wp-codex-contributions-' . $username ) ) ) {
 			
-			$results_url = 'http://codex.wordpress.org/api.php?action=query&list=usercontribs&ucuser=' . $username . '&uclimit=' . $limit . '&ucdir=older&format=xml';
-			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array('sslverify'=>false) ) );
+			$results_url = add_query_arg( array(
+				'action'	=>	'query',
+				'list'		=>	'usercontribs',
+				'ucuser'	=>	$username,
+				'uclimit'	=>	$limit,
+				'ucdir'		=>	'older',
+				'format'	=>	'php'
+			), 'http://codex.wordpress.org/api.php' );
+			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array( 'sslverify' => false ) ) );
+
+			$raw = maybe_unserialize( $results );
 			
-			/* Expected XML format is as follows:
-			 * <?xml version="1.0"?>
-			 * <api>
-			 *   <query>
-			 *     <usercontribs>
-			 *       <item user="Ericmann" pageid="21224" revid="109742" ns="0" title="Nginx" timestamp="2011-09-30T20:39:55Z" comment="External Links" />
-			 *       <item user="Ericmann" pageid="15227" revid="107245" ns="0" title="Switching to PHP5" timestamp="2011-07-08T21:41:56Z" comment="" />
-			 *       <item user="Ericmann" pageid="4580" revid="105196" ns="0" title="Using Subversion" timestamp="2011-05-19T15:47:29Z" comment="Add a resource." />
-			 *       <item user="Ericmann" pageid="4580" revid="104777" ns="0" title="Using Subversion" timestamp="2011-05-12T15:11:59Z" comment="Add a resource." />
-			 *       <item user="Ericmann" pageid="21198" revid="102162" ns="0" title="GSoC2011" timestamp="2011-03-17T13:58:14Z" comment="Mentors" />
-			 *       <item user="Ericmann" pageid="21198" revid="102100" ns="0" title="GSoC2011" timestamp="2011-03-15T13:55:20Z" minor="" comment="Mentors" />
-			 *       <item user="Ericmann" pageid="19084" revid="93082" ns="0" title="Post Types" timestamp="2010-09-18T17:08:51Z" comment="Fixing typos" />
-			 *     </usercontribs>
-			 *   </query>
-			 *   <query-continue>
-			 *     <usercontribs ucstart="2010-09-18T16:48:17Z"/>
-			 *   </query-continue>
-			 * </api>
+			/* Expected array format is as follows:
+			 * Array
+			 * (
+			 *     [query] => Array
+			 *         (
+			 *             [usercontribs] => Array
+			 *                 (
+			 *                     [0] => Array
+			 *                         (
+			 *                             [user] => Mbijon
+			 *                             [pageid] => 23000
+			 *                             [revid] => 112024
+			 *                             [ns] => 0
+			 *                             [title] => Function Reference/add help tab
+			 *                             [timestamp] => 2011-12-13T23:49:38Z
+			 *                             [minor] =>
+			 *                             [comment] => Functions typo fix
+			 *                         )
 			 **/
-			
-			$raw = new SimpleXMLElement( $results );
 			
 			$formatted = array();
 			
-			foreach( $raw->query->usercontribs->item as $item ) {
-				$newItem = array(
-					'title' => (string)$item['title'],
-					'description' => (string)$item['comment'],
-					'revision' => (int)$item['revid']
+			foreach( $raw['query']['usercontribs'] as $item ) {
+				$count = 0;
+				$clean_title = preg_replace( '/^Function Reference\//', '', (string) $item['title'], 1, $count );
+
+				$new_item = array(
+					'title'			=> $clean_title,
+					'description'	=> (string) $item['comment'],
+					'revision'		=> (int) $item['revid'],
+					'function_ref'	=> (bool) $count
 				);
-				array_push( $formatted, $newItem );
+				array_push( $formatted, $new_item );
 			}
 			
 			set_transient( 'wp-codex-contributions-' . $username, $formatted, 60 * 60 * 12 );
@@ -112,11 +132,17 @@ class WP_Core_Contributions{
 	}
 	
 	public static function get_codex_count( $username ) {
-		if ( $username == null ) return array();
+		if ( null == $username ) return array();
 
 		if ( false == ( $count = get_transient( 'wp-codex-contributions-count-' . $username ) ) ) {
 			
-			$results_url = 'http://codex.wordpress.org/api.php?action=query&list=users&ususers=' . $username . '&usprop=editcount&format=xml';
+			$results_url = add_query_arg( array(
+				'action'	=>	'query',
+				'list'		=>	'users',
+				'ususers'	=>	$username,
+				'usprop'	=>	'editcount',
+				'format'	=>	'xml'
+			), 'http://codex.wordpress.org/api.php' );
 			$results = wp_remote_retrieve_body( wp_remote_get( $results_url, array('sslverify'=>false) ) );
 			
 			/* Expected XML format is as follows:
@@ -131,7 +157,7 @@ class WP_Core_Contributions{
 			 **/
 			
 			$raw = new SimpleXMLElement( $results );
-			$count = (int)$raw->query->users->user["editcount"];
+			$count = (int) $raw->query->users->user["editcount"];
 			
 			set_transient( 'wp-codex-contributions-count-' . $username, $count, 60 * 60 * 12 );
 		}
@@ -141,4 +167,3 @@ class WP_Core_Contributions{
 }
 
 endif;
-?>
